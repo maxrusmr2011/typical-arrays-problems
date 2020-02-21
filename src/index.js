ceb8e39be02681ee7333aa507c39eec13f169d3f
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-02-22 01:41:55 +0200

    init: start typical-arrays-problems task

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+exports.min = function min(array) {
+    if (!array || !array.length) return 0;
+    return Math.min(...array);
+}
+
+exports.max = function max(array) {
+    if (!array || !array.length) return 0;
+
+    return Math.max(...array);
+}
+
+exports.avg = function avg(array) {
+    if (!array || !array.length) return 0;
+    return array.reduce((sum, item) => sum + item, 0) / array.length;
+}
